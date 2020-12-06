aaed953d3be18db4606f54f7eea7b61bcebee466
William Scullion <williamscullion@gmail.com>
2020-12-05 22:08:55 -0500

    refactor day 4

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -3,7 +3,6 @@
 use benchtest::benchtest;
 use itertools::Itertools;
 use std::collections::HashMap;
-use std::mem;
 
 const INPUT: &str = include_str!(concat!("data/day", 4, ".txt"));
 const REQUIRED_FIELDS: &[&str] = &["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
@@ -14,128 +13,73 @@ struct Passport {
 }
 
 impl Passport {
-    fn new() -> Self {
-        Self {
-            info: HashMap::new(),
+    fn new(lines: &[&str]) -> Self {
+        let mut info = HashMap::new();
+
+        for line in lines {
+            for pair in line.split_ascii_whitespace() {
+                let (k, v) = pair.split(":").next_tuple().unwrap();
+                info.insert(k.to_owned(), v.to_owned());
+            }
         }
+
+        Self { info }
     }
 
-    fn add_from_line(&mut self, line: &str) {
-        for pair in line.split_ascii_whitespace() {
-            let (k, v) = pair.split(":").next_tuple().unwrap();
-            self.info.insert(k.to_owned(), v.to_owned());
-        }
-    }
-
-    fn has_required_fields(&self) -> bool {
+    fn fields_present(&self) -> bool {
         REQUIRED_FIELDS
             .iter()
             .all(|field| self.info.contains_key(field.to_owned()))
     }
 
-    fn is_valid(&self) -> bool {
-        for field in REQUIRED_FIELDS {
-            match self.info.get_key_value(field.to_owned()) {
-                Some((k, v)) => {
-                    if !Self::validate_field(k, v) {
-                        return false;
-                    }
-                }
-                _ => return false,
-            }
-        }
-        return true;
+    fn fields_valid(&self) -> bool {
+        REQUIRED_FIELDS.iter().all(|field| {
+            self.info
+                .get_key_value(field.to_owned())
+                .map_or(false, |(k, v)| Self::validate_field(k, v))
+        })
     }
 
     fn validate_field(key: &str, value: &str) -> bool {
         match key {
-            "byr" => match value.parse::<u64>() {
-                Ok(n) => n >= 1920 && n <= 2002,
-                _ => false,
-            },
-            "iyr" => match value.parse::<u64>() {
-                Ok(n) => n >= 2010 && n <= 2020,
-                _ => false,
-            },
-            "eyr" => match value.parse::<u64>() {
-                Ok(n) => n >= 2020 && n <= 2030,
-                _ => false,
-            },
-            "hgt" => {
-                let chars = value.chars().collect::<Vec<_>>();
-
-                match chars.as_slice() {
-                    [n @ .., 'c', 'm'] => match n.into_iter().collect::<String>().parse::<u64>() {
-                        Ok(n) => n >= 150 && n <= 193,
-                        _ => false,
-                    },
-                    [n @ .., 'i', 'n'] => match n.into_iter().collect::<String>().parse::<u64>() {
-                        Ok(n) => n >= 59 && n <= 76,
-                        _ => false,
-                    },
-                    _ => false,
+            "byr" => matches!(value.parse(), Ok(1920..=2002)),
+            "iyr" => matches!(value.parse(), Ok(2010..=2020)),
+            "eyr" => matches!(value.parse(), Ok(2020..=2030)),
+            "hgt" => match value.chars().collect_vec().as_slice() {
+                [n @ .., 'c', 'm'] => {
+                    matches!(n.iter().collect::<String>().parse(), Ok(150..=193))
                 }
-            }
-            "hcl" => {
-                let chars = value.chars().collect::<Vec<_>>();
-
-                match chars.as_slice() {
-                    ['#', rest @ ..] => rest.iter().all(|c| matches!(c, '0'..='9' | 'a'..='f')),
-                    _ => false,
+                [n @ .., 'i', 'n'] => {
+                    matches!(n.iter().collect::<String>().parse(), Ok(59..=76))
                 }
-            }
+                _ => false,
+            },
+            "hcl" => match value.chars().collect_vec().as_slice() {
+                ['#', rest @ ..] => rest.iter().all(|c| matches!(c, '0'..='9' | 'a'..='f')),
+                _ => false,
+            },
             "ecl" => ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains(&value),
             "pid" => value.len() == 9 && value.parse::<u64>().is_ok(),
             _ => unreachable!(),
         }
     }
-
-    fn is_empty(&self) -> bool {
-        self.info.is_empty()
-    }
 }
 
-// Try in a more functional way
 fn puzzle_a(s: &str) -> usize {
-    let mut passports = vec![];
-    let mut passport = Passport::new();
-
-    for line in s.trim().lines() {
-        if line.is_empty() {
-            // TODO: mem replace alternative?
-            passports.push(mem::replace(&mut passport, Passport::new()));
-        }
-        passport.add_from_line(line);
-    }
-
-    if !passport.is_empty() {
-        passports.push(passport);
-    }
-
-    passports
-        .iter()
-        .filter(|passport| passport.has_required_fields())
+    let lines = s.trim().lines().collect_vec();
+    let groups = lines.as_slice().split(|l| l.is_empty());
+    groups
+        .map(Passport::new)
+        .filter(Passport::fields_present)
         .count()
 }
 
 fn puzzle_b(s: &str) -> usize {
-    let mut passports = vec![];
-    let mut passport = Passport::new();
-
-    for line in s.trim().lines() {
-        if line.is_empty() {
-            passports.push(mem::replace(&mut passport, Passport::new()));
-        }
-        passport.add_from_line(line);
-    }
-
-    if !passport.is_empty() {
-        passports.push(passport);
-    }
-
-    passports
-        .iter()
-        .filter(|passport| passport.is_valid())
+    let lines = s.trim().lines().collect_vec();
+    let groups = lines.as_slice().split(|l| l.is_empty());
+    groups
+        .map(Passport::new)
+        .filter(Passport::fields_valid)
         .count()
 }
 
